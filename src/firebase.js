89a35c13e06f362dfd95302d24f93142db4d7ffc
SumitShinde0702 <sumitsanjayshinde0702@gmail.com>
2025-10-15 10:51:08 +0800

    Add Firebase configuration

--- src/firebase.js
+++ src/firebase.js
@@ -17,16 +17,6 @@ import {
   addDoc,
 } from "firebase/firestore";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDIXJ5YT7hoNbBFqK3TBcV41-TzIO-7n7w",
-//   authDomain: "fir-auth-6edd8.firebaseapp.com",
-//   projectId: "fir-auth-6edd8",
-//   storageBucket: "fir-auth-6edd8.appspot.com",
-//   messagingSenderId: "904760319835",
-//   appId: "1:904760319835:web:44fd0d957f114b4e51447e",
-//   measurementId: "G-Q4TYKH9GG7",
-// };
-
 const firebaseConfig = {
   apiKey: "AIzaSyDBtP1zDDwEOpkLQmoBpqCuUxbgT1rOmyg",
   authDomain: "tutorial6-f9e22.firebaseapp.com",
